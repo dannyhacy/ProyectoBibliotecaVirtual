14e417ead4412c67d628b9b367437281f08c492c
Danny Hamilton Cerna Yaya <dannyhacy@gmail.com>
2025-02-21 00:58:16 -0500

    version 1.4 Se corrigió todos los nombres de variables de los CRUD...

--- Controllers/Libros/LibroController.cs
+++ Controllers/Libros/LibroController.cs
@@ -17,16 +17,16 @@ namespace Biblioteca.Controllers.Libros
 
         public async Task<IActionResult> Listar()
         {
-            var Libro = await _LibroContext.Libros
+            var libro = await _LibroContext.Libros
                 .Include(a => a.Autor)
                 .Include(e => e.Editorial)
                 .Include(g => g.Genero)
                 .Include(i => i.Idioma)
                 .Include(ul => ul.UbicacionLibro)
                 .Include(el => el.EstadoLibro)
-                .Include(el => el.EstadoPrestamo)
+                .Include(ep => ep.EstadoPrestamo)
                 .ToListAsync();
-            return View(Libro);
+            return View(libro);
         }
 
         [HttpGet]
@@ -72,7 +72,6 @@ namespace Biblioteca.Controllers.Libros
             {
                 ModelState.AddModelError("", "Ocurrio un error al momento de Editar");
             }
-
             var libro = await _LibroContext.Libros.FirstOrDefaultAsync(l => l.Id == id);
             if (libro == null)
             {
@@ -153,7 +152,6 @@ namespace Biblioteca.Controllers.Libros
             }
             catch (Exception)
             {
-                // Manejo de excepciones
                 ModelState.AddModelError("", "No se puede eliminar Libro");
             }
             return View(Libro);
